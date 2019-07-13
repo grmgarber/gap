eaed901734ca0721920e51800f9b42c86693b515
grmgarber <gr.m.garber@gmail.com>
2019-07-13 17:33:11 -0400

    first spec passes

--- spec/requests/create_spec.rb
+++ spec/requests/create_spec.rb
@@ -44,26 +44,23 @@ RSpec.describe "API V1 Quotes", type: 'request' do
         }
       end
 
-      let(:more_params) do
-        "{\"json\":{\"address\":{\"street\":\"1212 High St\",\"city\":\"Chicago\",\"state\":\"IL\",\"postal_code\":\"60654\"},\"rent_roll\":[{\"monthly_rent\":6000,\"unit_number\":\"1A\",\"vacancy\":false,\"nbr_bedrooms\":6,\"nbr_bathrooms\":7,\"annual_total\":72000},{\"monthly_rent\":5000,\"unit_number\":\"2A\",\"vacancy\":true,\"nbr_bedrooms\":5,\"nbr_bathrooms\":4,\"annual_total\":60000}],\"expenses\":{\"marketing\":1500,\"tax\":20000,\"insurance\":4500,\"repairs\":10000,\"admin\":2000,\"payroll\":0,\"utility\":5400,\"management\":1700},\"cap_rate\":0.1}}"
-      end
-
       it "creates a new quote successfully" do
-        expect { post "/api/v1/quotes", params: valid_params }.to change(Quote, :count).by(1)
-        expect(response).to have_http_status :created
+        post "/api/v1/quotes", params: valid_params.to_json,
+                               headers: {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+
+        rb_resp = JSON.parse(response.body).except!("token") # token is random, so kill it
+        expect(rb_resp.to_json).to eq("{\"debt_rate\":0.04124,\"loan_amount\":\"869000.0\",\"monthly_payment\":\"8849.51\"}")
       end
-
-      it "again" do
-        expect { post "/api/v1/quotes", params: more_params }.to change(Quote, :count).by(1)
-        # expect(response).to have_http_status :created
-      end
-
-
     end
 
     context "with invalid parameters" do
-      # testing for validation failures is just as important!
-      # ...
+      it "returns an error when address is missing" do
+        post "/api/v1/quotes", params: valid_params.except().to_json,
+             headers: {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+
+        rb_resp = JSON.parse(response.body).except!("token") # token is random, so kill it
+        expect(rb_resp.to_json).to eq("{\"debt_rate\":0.04124,\"loan_amount\":\"869000.0\",\"monthly_payment\":\"8849.51\"}")
+      end
     end
   end
 end
